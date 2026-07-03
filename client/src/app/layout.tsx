e8564c57f202dc025b0be1f313629ed6c3ccdd03
sajib9988 <sajib.biswas9988@gmail.com>
2026-07-03 10:53:01 +0600

    uuu

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col ">
+      <body className={`${geistSans.className}min-h-full flex flex-col`}>
         {children}
         <Toaster position="top-center" />
       </body>
