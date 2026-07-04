63d9a8459af095d5d44162fcc7c67691ac6211c7
sajib9988 <sajib.biswas9988@gmail.com>
2026-07-05 05:36:04 +0600

    u

--- client/src/app/(public)/page.tsx
+++ client/src/app/(public)/page.tsx
@@ -72,8 +72,8 @@ const pageRes = await getPageBySlug("home-page"); // ✅ direct call
 
 
 {/* ── Section 3: Our Ecosystem ── */}
-<section className=" mx-auto py-16">
-  <div className="max-w-7xl px-4 sm:px-6 lg:px-8">
+<section className="bg-slate-950 max-w-7xl py-16">
+  <div className="max-auto px-4 sm:px-6 lg:px-8">
     <EcosystemSection companies={companies} />
   </div>
 </section>
