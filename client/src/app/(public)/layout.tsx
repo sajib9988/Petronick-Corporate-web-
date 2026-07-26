16f96a952eeb731c3b5c6b6c1598ec57e5fe2558
sajib9988 <sajib.biswas9988@gmail.com>
2026-07-26 20:34:35 +0600

    hhgg

--- client/src/app/(public)/layout.tsx
+++ client/src/app/(public)/layout.tsx
@@ -6,25 +6,13 @@ import { ReactNode } from 'react';
 export default function PublicLayout({ children }: { children: ReactNode }) {
   return (
 
-    // ata ager style chilo
-    // <div className='bg-sky-100 '>
-    //   <Navbar/>
-    //   {children}
-    //   <Footer />
-    // </div>
-
-    // ata notun style
- <div className="relative min-h-screen">
-      {/* 🔵 Full Website Background Gradient - sobar pisoney */}
-      <div className="fixed inset-0 -z-50 bg-gradient-to-br from-sky-100 via-blue-50 to-white" />
-      
-      {/* Content */}
-      <Navbar />
-      <main className="relative z-10">
-        {children}
-      </main>
+    
+    <div className='bg-sky-100 '>
+      <Navbar/>
+      {children}
       <Footer />
     </div>
+
 
 
 
