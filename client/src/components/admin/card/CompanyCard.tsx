ab47199c72f3dcb8b2cbed80c3e4e2b4c8fcd612
sajib9988 <sajib.biswas9988@gmail.com>
2026-09-12 20:01:51 +0600

    hh

--- client/src/components/admin/card/CompanyCard.tsx
+++ client/src/components/admin/card/CompanyCard.tsx
@@ -242,8 +242,8 @@ export default function CompanyCard({
           <div
             className={`
               flex
-              h-14
-              w-14
+              h-18
+              w-18
               shrink-0
               items-center
               justify-center
