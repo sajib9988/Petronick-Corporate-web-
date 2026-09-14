3781d03b2c1b8f703356fc618c18ec905edb1729
sajib9988 <sajib.biswas9988@gmail.com>
2026-09-14 20:12:45 +0600

    hh

--- client/src/components/admin/card/CompanyCard.tsx
+++ client/src/components/admin/card/CompanyCard.tsx
@@ -168,7 +168,7 @@ export default function CompanyCard({ company, index = 0 }: CompanyCardProps) {
 </p>
 
        {/* FOOTER */}
-<div className="mt-auto flex w-full items-center justify-between gap-7 pt-3">
+<div className="mt-auto flex w-full items-center justify-between gap-9 pt-3">
   <Link
     href={`/companies/${company.id}`}
     className="
