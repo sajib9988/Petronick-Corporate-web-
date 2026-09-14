785ce190b78b5bc0993a170c4a948ccbc855b50b
sajib9988 <sajib.biswas9988@gmail.com>
2026-09-14 21:06:43 +0600

    gg

--- client/src/components/home-components/ClosingSection.tsx
+++ client/src/components/home-components/ClosingSection.tsx
@@ -46,9 +46,9 @@ export default function ClosingSection({ content }: ClosingSectionProps) {
   ].filter(Boolean);
 
   return (
-    <section className="relative py-10  sm:px-10 lg:px-12 rounded-2xloverflow-hidden">
-      <div className="pointer-events-none absolute -top-24 left-1/4 h-64 w-64 rounded-full bg-amber-500/10 blur-3xl" />
-      <div className="pointer-events-none absolute -bottom-24 right-1/4 h-64 w-64 rounded-full bg-amber-500/10 blur-3xl" />
+    <section className="relative   sm:px-10  rounded-2xloverflow-hidden">
+      <div className="pointer-events-none absolute -top-24 left-1/4 h-64 w-64 rounded-full  blur-3xl" />
+      <div className="pointer-events-none absolute -bottom-24 right-1/4 h-64 w-64 rounded-full" />
 
       <motion.div
         initial="hidden"
