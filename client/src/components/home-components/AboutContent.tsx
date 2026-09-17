aac7d214b4207f1a10a8c6016b77d9909e48430f
sajib9988 <sajib.biswas9988@gmail.com>
2026-09-17 19:35:00 +0600

    gg

--- client/src/components/home-components/AboutContent.tsx
+++ client/src/components/home-components/AboutContent.tsx
@@ -710,7 +710,7 @@ export default function AboutContent({
             PART 5 — VALUES (How We Operate — 5-step flow)
         ====================================================== */}
 
-        <section className="py-8 bg-slate-50 w-full">
+        <section className="py-4  bg-slate-50 w-full">
             <div className="mx-auto w-full max-w-350 px-6 sm:px-10 lg:px-14 py-16 sm:py-20">
 
             <motion.div
