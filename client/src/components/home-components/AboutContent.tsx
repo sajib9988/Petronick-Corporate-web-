fabede4d24eca09887ba8db8d0da435906e729c9
sajib9988 <sajib.biswas9988@gmail.com>
2026-09-19 21:57:18 +0600

    hh

--- client/src/components/home-components/AboutContent.tsx
+++ client/src/components/home-components/AboutContent.tsx
@@ -724,7 +724,7 @@ export default function AboutContent({
         <section className="py-2  bg-slate-50 w-full">
 
             <Container>
-            <div className=" w-full  px-6 sm:px-10  py-16 sm:py-20">
+           <div className="w-full py-16 sm:py-20">
 
             <motion.div
               initial="hidden"
